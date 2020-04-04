ad2b22dc713d14ec88bfbc02d1cbf074b79fcb8b
menvol3 <disbalance@ukr.net>
2020-04-04 17:30:05 +0300

    commit

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,19 +1,25 @@
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * ArrayUtil exercises.
  */
 public class ArrayUtil {
 
-  /**
-   * Finds the common elements between two arrays (String values).
-   *
-   * @param array1 first array
-   * @param array2 second array
-   * @return String array with common elements
-   */
-  public static String[] findCommon(String[] array1, String[] array2) {
-    // TODO fill in code here
-    return new String[0];
-  }
+    /**
+     * Finds the common elements between two arrays (String values).
+     *
+     * @param array1 first array
+     * @param array2 second array
+     * @return String array with common elements
+     */
+    public static String[] findCommon(String[] array1, String[] array2) {
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    System.out.println(array1[i]);
+                }
+            }
+        }
+        return new String[0];
+    }
 }
