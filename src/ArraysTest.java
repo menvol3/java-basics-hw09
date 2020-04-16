dc1f62f22cb8fc858ad30ab9d161ddf652fd92b0
menvol3 <disbalance@ukr.net>
2020-04-16 13:25:27 +0300

    Update ArraysTest.java
    fixed

--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -5,27 +5,36 @@ import org.testng.annotations.Test;
 
 public class ArraysTest {
 
-  /**
-   * Test data for common array elements.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "ArrayUtil")
-  public static Object[][] arrays() {
-    return new Object[][]{
-        {
-            new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
-            new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
-            new String[]{"JAVA", "SQL"}
-        }
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for common array elements.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "ArrayUtil")
+    public static Object[][] arrays() {
+        return new Object[][]{
+                {
+                        new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+                        new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+                        new String[]{"JAVA", "SQL"}
+                },
+                {
+                        new String[]{"orange", "apple", "kiwi", "garlic", "meat"},
+                        new String[]{"orange", "raspberry", "garlic", "meat", "fish"},
+                        new String[]{"orange", "garlic", "meat"}
+                },
+                {
+                        new String[]{"here", "is", "some", "text", "."},
+                        new String[]{"some", "text", "above", "."},
+                        new String[]{"some", "text", "."}
+                }
+        };
+    }
 
-  @Test(dataProvider = "ArrayUtil")
-  public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+    @Test(dataProvider = "ArrayUtil")
+    public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
 
-    assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
-        "Common elements are not correct");
-  }
+        assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
+                "Common elements are not correct");
+    }
 }
